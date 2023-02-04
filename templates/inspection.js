939e5c28afdb2cd67e8b326eb5a7d5b0fab85e94
Wendy Qi <wendy.qi@berkeley.edu>
2023-02-04 13:14:37 -0800

    drop work

--- templates/inspection.js
+++ templates/inspection.js
@@ -1,16 +1,21 @@
 
 function allowDrop(ev) {
     ev.preventDefault();
+    console.log("allowDrop");
   }
   
   function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
+    console.log("drag");
+
   }
   
   function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
+    console.log("drop");
+
   }
 
   
